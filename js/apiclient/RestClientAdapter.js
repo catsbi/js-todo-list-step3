dd1907178794b01548217fe965777cd5ad43a725
jho2301 <jho2301@gmail.com>
2020-09-26 21:23:02 +0900

    전환오3

--- js/apiclient/RestClientAdapter.js
+++ js/apiclient/RestClientAdapter.js
@@ -1,5 +1,11 @@
 import { RestClient } from "./RestClient.js";
 
 export const RestClinetAdapter = class extends RestClient {
-
+  constructor() {
+    super(BASE_URL);
+  }
+  execute() {
+    // 로딩바추가
+    super.execute();
+  }
 };
